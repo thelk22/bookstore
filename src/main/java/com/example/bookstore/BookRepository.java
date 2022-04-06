18def41b3f7dbd62f14d7b50ff6736eb862a15b5
Luke Kirwan <40961351+thelk22@users.noreply.github.com>
2022-04-06 19:57:54 +0100

    create stubbed list of books from constructor

--- src/main/java/com/example/bookstore/BookRepository.java
+++ src/main/java/com/example/bookstore/BookRepository.java
@@ -8,13 +8,19 @@ import java.util.List;
 @Service
 public class BookRepository implements IBookRepository  {
 
+    private List<Book> books;
+
+    BookRepository() {
+        ArrayList<Book> books = new ArrayList<>();
+        books.add(new Book("LOTR", "J.R.R. Tolkien"));
+        books.add(new Book("A Gentleman in Moscow", "Amor Towles"));
+        books.add(new Book("Outliers", "Malcolm Gladwell"));
+        this.books = books;
+    }
+
     @Override
     public List<Book> findAll() {
-        ArrayList<Book> bookList = new ArrayList<>();
-        bookList.add(new Book("LOTR", "J.R.R. Tolkien"));
-        bookList.add(new Book("A Gentleman in Moscow", "Amor Towles"));
-        bookList.add(new Book("Outliers", "Malcolm Gladwell"));
-        return bookList;
+        return books;
     }
 
 }
